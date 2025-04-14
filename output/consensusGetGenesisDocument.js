61794ff37a4f022e4bd46de9e7218f18318af6ce
lukaw3d <lukajeran@oasislabs.com>
2025-04-14 03:57:41 +0200

    node generate1.js

---
+++ output/consensusGetGenesisDocument.js
@@ -0,0 +1,12 @@
+
+import oasis from '@oasisprotocol/client'
+import xhr2 from 'xhr2'
+import {out, parseGrpc} from '../parseGrpc.js'
+global.XMLHttpRequest = xhr2
+const nic = new oasis.client.NodeInternal('https://grpc.oasis.io')
+try {
+  console.log('const output = ')
+  out(await nic.consensusGetGenesisDocument(undefined))
+} catch (err) {
+  console.log('error', err)
+}
---
+++ output/registryGetNodeByConsensusAddress.js
@@ -0,0 +1,23 @@
+
+import oasis from '@oasisprotocol/client'
+import xhr2 from 'xhr2'
+import {out, parseGrpc} from '../parseGrpc.js'
+global.XMLHttpRequest = xhr2
+const nic = new oasis.client.NodeInternal('https://grpc.oasis.io')
+try {
+  console.log('const output = ')
+  out(await nic.registryGetNodeByConsensusAddress({
+      height: 0,
+      epoch: 34315,
+      round: 8433003,
+      runtime_id: oasis.misc.fromHex('000000000000000000000000000000000000000000000000f80306c9858e7279' /* sapphireConfig.mainnet.runtimeId */),
+      runtimeID: oasis.misc.fromHex('000000000000000000000000000000000000000000000000f80306c9858e7279' /* sapphireConfig.mainnet.runtimeId */),
+      owner: oasis.staking.addressFromBech32('oasis1qz78ap0456g2rk7j6rmtvasc9v2kjhz2s58qgj90' /* my mainnet dev addr */),
+      beneficiary: oasis.staking.addressFromBech32('oasis1qz78ap0456g2rk7j6rmtvasc9v2kjhz2s58qgj90' /* my mainnet dev addr */),
+      address: oasis.staking.addressFromBech32('oasis1qq3xrq0urs8qcffhvmhfhz4p0mu7ewc8rscnlwxe' /* stakefish */),
+      include_suspended: true,
+      id: 1,
+    }))
+} catch (err) {
+  console.log('error', err)
+}
